00a3de00518de41604201d47741b93561bac825c
Kyltetran <138639043+Kyltetran@users.noreply.github.com>
2024-11-21 16:25:51 +0700

    fix4

--- main.py
+++ main.py
@@ -37,6 +37,29 @@ print("API Key loaded successfully.")
 app = Flask(__name__)
 app.secret_key = '123456'
 
+from langchain_openai import OpenAIEmbeddings
+
+def get_embedding_function():
+    # embeddings = OllamaEmbeddings(
+    #     model="llama3.2",
+    # )
+    # # embeddings = OllamaEmbeddings(model="nomic-embed-text")0
+    # return embeddings
+
+    # Get the API key from environment variables
+    # api_key = os.getenv("OPEN_API_KEY")
+    # print("Loaded API Key:", os.getenv("OPEN_API_KEY"))
+    # if not api_key:
+    #     raise ValueError("OpenAI API key not found in environment variables.")
+
+    embeddings = OpenAIEmbeddings(
+            model="text-embedding-3-large",
+            # openai_api_key=api_key
+        )
+        # embeddings = OllamaEmbeddings(model="nomic-embed-text")0
+    return embeddings
+
+
 # Define the route for the frontend
 @app.route('/')
 def index():
